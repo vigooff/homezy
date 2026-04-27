e0187b9250cc2bb40aafbe718bec411222f1e094
vigooff <vgooffical.com>
2026-04-27 11:57:32 +0700

    ease agent fix

--- src/templates/AgentSection.tsx
+++ src/templates/AgentSection.tsx
@@ -49,7 +49,7 @@ export const AgentSection = () => {
     visible: {
       opacity: 1,
       y: 0,
-      transition: { duration: 0.8, ease: [0.25, 0.46, 0.45, 0.94] }
+      transition: { duration: 0.8, ease: [0.25, 0.46, 0.45, 0.94] as [number, number, number, number] }
     }
   };
 
